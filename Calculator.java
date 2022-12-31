a7d3fa269a19ef6b1e75b4348dce8aabeae7df4d
Somnath Gonte <105125522+somnathgonte@users.noreply.github.com>
2022-12-31 16:15:46 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,33 @@
+//Mathematical operation of two numbers
+
+import java.util.*;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        Scanner Sobj = new Scanner(System.in);
+        System.out.print("Enter First number:");
+        int iNo1 = Sobj.nextInt();
+
+        System.out.print("Enter second number:");
+        int iNo2 = Sobj.nextInt();
+
+        int Add = iNo1 + iNo2;
+        System.out.println(Add);
+
+        int Sub = iNo1 - iNo2;
+        System.out.println(Sub);
+
+        int Multi = iNo1 * iNo2;
+        System.out.println(Multi);
+
+        int Divi = iNo1 / iNo2;
+        System.out.println(Divi);
+
+        int Mod = iNo1 % iNo2;
+        System.out.println(Mod);
+
+    }
+
+}
