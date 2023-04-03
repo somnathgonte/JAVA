97f46e79249e79f02ee4a68803fad3ce72904a3c
Somnath Gonte <105125522+somnathgonte@users.noreply.github.com>
2023-04-03 23:45:49 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -2,9 +2,11 @@
 
 import java.util.*;
 
-public class Calculator {
+public class Calculator 
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
 
         Scanner Sobj = new Scanner(System.in);
         System.out.print("Enter First number:");
