5f602af30f9fa713b0990a0c51f7354a91821ab8
Somnath Gonte <105125522+somnathgonte@users.noreply.github.com>
2023-07-25 00:49:38 +0530

    Add files via upload

---
+++ Constructor2.java
@@ -0,0 +1,15 @@
+//No-argument constructor.
+//This constructor is created by programmer.
+//It has no arguments.
+
+public class Constructor2 
+{
+    Constructor2()                 //User defined constructor.
+    {
+        System.out.println("No argument Constructor.");
+    }
+    public static void main(String[] args) 
+    {
+        Constructor2 ct = new Constructor2();   
+    }
+}
