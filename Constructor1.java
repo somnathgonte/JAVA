6144d433488db758a8538c04cef3fdec2eda5776
Somnath Gonte <105125522+somnathgonte@users.noreply.github.com>
2023-07-25 00:49:24 +0530

    Add files via upload

---
+++ Constructor1.java
@@ -0,0 +1,16 @@
+//Default Constructor
+//It is created by compiler automatically.
+//It has no arguments.
+
+public class Constructor1 
+{
+    int i;
+    String s;
+
+    public static void main(String[] args)
+    {
+        Constructor1 ct = new Constructor1();
+        System.out.println(ct.i);
+        System.out.println(ct.s);
+    }
+}
